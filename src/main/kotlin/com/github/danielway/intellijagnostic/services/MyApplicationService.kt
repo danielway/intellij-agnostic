e05dd3203243c2d988da7433d070c9edad3d99c5
GitHub Action <action@github.com>
2022-12-25 18:00:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/danielway/intellijagnostic/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/danielway/intellijagnostic/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.danielway.intellijagnostic.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.danielway.intellijagnostic.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/danielway/intellijagnostic/services/MyProjectService.kt
+++ src/main/kotlin/com/github/danielway/intellijagnostic/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.danielway.intellijagnostic.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.danielway.intellijagnostic.MyBundle
 
 class MyProjectService(project: Project) {
 
