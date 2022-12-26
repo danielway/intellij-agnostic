dfd898651bff828f4573589e7f7ad26f654e1af0
Daniel Way <projects@danieldway.com>
2022-12-26 15:56:56 -0600

    Add Agnostic language icon

---
+++ src/main/kotlin/org/intellij/sdk/language/AgnosticIcons.kt
@@ -0,0 +1,9 @@
+package org.intellij.sdk.language
+
+import com.intellij.openapi.util.IconLoader
+
+class AgnosticIcons {
+    companion object {
+        val FILE = IconLoader.getIcon("/icons/agnostic-icon.png", Companion::class.java)
+    }
+}
